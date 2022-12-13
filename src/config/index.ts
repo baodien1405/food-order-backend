08647ad90b68ea5f52e6635e00d5d74d8a538482
Dien Cap <diencb@innotech-vn.com>
2022-12-13 13:30:44 +0700

    Update url mongoDB production and ignore dist folder

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const MONGO_URI = 'mongodb://localhost:27017/online_food_delivery'
-// export const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.1rzqqle.mongodb.net/?retryWrites=true&w=majority`
+// export const MONGO_URI = 'mongodb://localhost:27017/online_food_delivery'
+export const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.1rzqqle.mongodb.net/?retryWrites=true&w=majority`
 export const APP_SECRET = process.env.APP_SECRET
 export const PORT = process.env.PORT || 8000
